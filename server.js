1592b762c1d78c8e3514812ae6a1942ffeeaba81
Ben Sunshine-Hill <besuns@microsoft.com>
2017-04-06 09:23:22 +0100

    better response

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ app.use(bodyParser.json())
 
 app.post('/alexa', function (req, res, next) {
     console.log(req.body)
-    res.status(200).json(req.body)
+    res.status(200).json({
+        version: "1.0",
+        response: {
+            outputSpeech: {
+                type: "PlainText",
+                text: "beep beep"
+            },
+            shouldEndSession: true
+        }
+    })
 })
 
 app.listen(process.env.port || 3434, function () {
