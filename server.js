4e601878b7d1b1471fdb7bb29a50a4cc64f64b69
Ben Sunshine-Hill <besuns@microsoft.com>
2017-04-06 09:14:22 +0100

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿var express = require("express")
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.post('/alexa', function (req, res, next) {
+    console.log(req.body)
+    res.status(200).json(req.body)
+})
+
+app.listen(process.env.port || 3434, function () {
+    console.log('Server listening on port', process.env.port || 3434)
+})
