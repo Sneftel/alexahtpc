c60a990cb35e68ae9a29fd66e20f588b2e41e11e
Ben Sunshine-Hill <besuns@microsoft.com>
2017-04-06 11:47:42 +0100

    id checking, and rename endpoint

--- server.js
+++ server.js
@@ -8,29 +8,43 @@ app.use(bodyParser.json())
 var cecip = process.env.CUSTOMCONNSTR_cecip
 var cecport = process.env.CUSTOMCONNSTR_cecport
 
-app.post('/alexa', function (req, res, next) {
+function isValidRequest(json) {
+    try {
+        return json.session.application.applicationId == process.env.ALEXA_APPLICATION_ID
+    }
+    catch (err) {
+        return false
+    }
+}
+
+app.post('/theatermode', function (req, res, next) {
     console.log(req.body)
 
-    socket = dgram.createSocket('udp4')
-    socket.send("", cecport, cecip, function (err) {
-        if (err) {
-            console.log("Error when sending datagram:", err)
-            return next(err)
-        }
-        console.log("Sent datagram.")
-        socket.close()
-    })
+    if (isValidRequest(req.body)) {
+        res.status(200).json({
+            version: "1.0",
+            response: {
+                outputSpeech: {
+                    type: "PlainText",
+                    text: "beep beep"
+                },
+                shouldEndSession: true
+            }
+        })
 
-    res.status(200).json({
-        version: "1.0",
-        response: {
-            outputSpeech: {
-                type: "PlainText",
-                text: "beep beep"
-            },
-            shouldEndSession: true
-        }
-    })
+        socket = dgram.createSocket('udp4')
+        socket.send("", cecport, cecip, function (err) {
+            if (err) {
+                console.log("Error when sending datagram:", err)
+                return next(err)
+            }
+            console.log("Sent datagram.")
+            socket.close()
+        })
+    }
+    else {
+        res.status(401)
+    }
 })
 
 app.listen(process.env.port || 3434, function () {
